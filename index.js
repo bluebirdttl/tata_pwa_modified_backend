eab4323af9843d278faa22543d640d29ad08ded6
Sahil Ganbhoj <ganbhojsahil@gmail.com>
2025-12-01 18:42:30 +0530

    New Security Changes

--- index.js
+++ index.js
@@ -25,11 +25,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Request Logging Middleware
-app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-});
+
 
 // -----------------------------
 // Backend Health Check
@@ -47,7 +43,7 @@ app.use('/api/projects', projectRoutes);
 
 // 404 Handler
 app.use((req, res) => {
-    console.log(`[404] Route not found: ${req.method} ${req.url}`);
+
     res.status(404).send('Route not found');
 });
 
